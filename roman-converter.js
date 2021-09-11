860f3a8ac96ed76c18eba227b51642c804317709
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-09-11 17:52:15 +0800

    Final: Add function for converting roman char

--- roman-converter.js
+++ roman-converter.js
@@ -1,49 +1,21 @@
-let roman = ''
-function convertToRoman(num) {
-    while(num !== 0) {
-        if (num >= 1000) {
-            roman += 'M'
-            num -= 1000
-        } else if (num >= 999) {
-            roman += 'C'
-            num -= 900
-        } else if (num >= 500) {
-            roman += 'D'
-            num -= 500
-        } else if (num >= 499) {
-            roman += 'C'
-            num -= 400
-        } else if (num >= 100) {
-            roman += 'C'
-            num -= 100
-        } else if (num >= 99) {
-            roman += 'X'
-            num -= 90
-        } else if (num >= 50) {
-            roman += 'L'
-            num -= 50
-        } else if (num >= 49) {
-            roman += 'X'
-            num -= 40
-        } else if (num >= 10) {
-            roman += 'X'
-            num -= 10
-        } else if (num >= 9) {
-            roman += 'I'
-            num -= 1
-        } else if (num >= 5) {
-            roman += 'V'
-            num -= 5
-        } else if (num >= 4) {
-            roman += 'I'
-            num -= 1
-        } else {
-            roman += 'I'
-            num -= 1
-        }
-    }
-    return roman
-}
-   
-console.log(convertToRoman(1200)) //DCCCXCI
-console.log(convertToRoman(3999)) //MMMCMXCIX
+const romanNumList = {M:1000,CM:900, D:500,CD:400, C:100, XC:90,L:50, XL: 40, X:10, IX:9, V:5, IV:4, I:1};
+let roman = "";
+function arabicToRoman(number){ 
+    let a;
+      for(let key in romanNumList){
+           /* use division operator to get how many each char 
+           in the object value will be store in a */
+          a = Math.floor(number / romanNumList[key]); //3999/1000 = 3 
+          if(a >= 0){
+              for(let i = 0; i < a; i++) { // a = 3 
+                roman += key; /* MMM we got 3 m here since in the
+                                first loop since it is ture */
+              }
+            }
+            // using modulo return the remaining number
+          number = number % romanNumList[key];
+      }
+    return roman;
+  }
+
+console.log(arabicToRoman(44))
